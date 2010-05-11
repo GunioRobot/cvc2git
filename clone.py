2a276ffb3215722526a10527e492a4e9ebc375fc
Zhang Sen <zh.jesse@gmail.com>
2010-05-11 15:07:41 +0800

    Allow :source in clone
    And don't allow cloning to existing dir

--- clone.py
+++ clone.py
@@ -44,16 +44,26 @@ def parse_history():
 
     return revisions
 
+def parse_package_name(full_version):
+    '''Parse package name from a full version string'''
+    s = full_version.split("=", 1)[0]
+    if ":" in s: # this is a trove name, like 'pkg:source'
+        return s.split(":", 1)[0]
+    return s
+
 def checkout(trove, dest):
-    print "checking out %s" % trove, ("to " + dest) if dest else ""
-    extra = ("--dir=" + dest) if dest else ""
-    s = getstatusoutput("cvc checkout %s %s" % (trove, extra))
-    if s[0] == 0:
-        # TODO handle clone dummy-test:source=/jesse.rpath.org@fl:2-devel
-        dir = dest if dest else trove.split("=")[0]
-        return os.path.abspath(dir)
-    print "cvc co fails:", s[1]
-    return None
+    if not dest:
+        dest = parse_package_name(trove)
+    if os.path.isdir(dest) and os.listdir(dest):
+        print "Error: directory %s already exists and is not empty" % dest
+        sys.exit()
+
+    print "checking out %s to %s" % (trove, dest)
+    s = getstatusoutput("cvc checkout --dir=%s %s" % (dest, trove))
+    if s[0] != 0:
+        print "cvc co fails:", s[1]
+        sys.exit()
+    return dest
 
 def get_file_list():
     '''recognize the files in <package>:source'''
@@ -81,8 +91,6 @@ def commit_to_git(revisions):
 
 def do_clone(trove, dest=None):
     workdir = checkout(trove, dest)
-    if not workdir:
-        sys.exit()
 
     # change to work directory
     os.chdir(workdir)
