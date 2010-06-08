8c5b793c53f520a309fd4bd84fd1cd74b3ddb57e
Zhang Sen <zh.jesse@gmail.com>
2010-06-08 23:32:18 +0800

    No need to 'cvc update' for 'cvc2git pull'
    cvc log always returns the whole history, even if remote has updates. So there
    is no need to update the package in order to get the history.

--- pull.py
+++ pull.py
@@ -12,20 +12,12 @@ from utils import (
 def do_pull():
     current_rev = read_local_version()
     print "local revision:", current_rev
-    s = getstatusoutput("cvc update")
-    if s[0] != 0:
-        print "error with cvc update:", s[1]
-        sys.exit()
-
-    remote_rev = read_local_version()
-    print "remote revision:", remote_rev
-
-    if current_rev == remote_rev:
-        print "no changes found"
-        return
 
     revisions = parse_history()
     ix = locate_rev_in_log(revisions, current_rev)
+    if not ix:
+        print "no changes found"
+        return
     commit_to_git(revisions[:ix])
     print "done"
 
